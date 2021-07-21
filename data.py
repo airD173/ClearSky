d508ce6d252714b03402e7bf63887c132043fcc2
airD173 <adey1731@gmail.com>
2021-07-21 17:10:29 -0400

    Sent alert data to embeds and other optimizations

--- data.py
+++ data.py
@@ -10,21 +10,21 @@ def main(zipcode):
     latitude = zipdb[zipdb['zip'] == zipcode]['latitude'].values[0]
     longitude = zipdb[zipdb['zip'] == zipcode]['longitude'].values[0]
     coord = requests.get(f'https://api.weather.gov/points/{latitude},{longitude}').json()
+    alertdata = requests.get('https://api.weather.gov/alerts/active?zone=' + requests.get(coord['properties']['county']).json()['properties']['id']).json()
+    forecast = requests.get(coord['properties']['forecast']).json()['properties']['periods']
+    
+    if len(alertdata['features']) == 0:
+        alerts = None
+    else:
+        alerts = alertdata['features']
+            
 
-    alerts = requests.get('https://api.weather.gov/alerts/active?zone=' + requests.get(coord['properties']['county']).json()['properties']['id']).json()
-
-
-    forecast = requests.get(coord['properties']['forecast']).json()
-    name = forecast['properties']['periods'][0]['name']
-    temp = forecast['properties']['periods'][0]['temperature']
-    thumb = icon.main(forecast['properties']['periods'][0]['icon'])
-    short = forecast['properties']['periods'][0]['shortForecast']
-    detailed = forecast['properties']['periods'][0]['detailedForecast']
-    data1 = (zipcode, name, temp, thumb, short, detailed)
-    name = forecast['properties']['periods'][1]['name']
-    temp = forecast['properties']['periods'][1]['temperature']
-    thumb = icon.main(forecast['properties']['periods'][1]['icon'])
-    short = forecast['properties']['periods'][1]['shortForecast']
-    detailed = forecast['properties']['periods'][1]['detailedForecast']
-    data2 = (None, name, temp, thumb, short, detailed)
+    
+    data1 = (zipcode, forecast[0]['name'], forecast[0]['temperature'], icon.main(forecast[0]['icon']), forecast[0]['shortForecast'], forecast[0]['detailedForecast'])
+    #data1 = (zipcode, name, temp, thumb, short, detailed)
+    
+    data2 = (alerts, forecast[1]['name'], forecast[1]['temperature'], icon.main(forecast[1]['icon']), forecast[1]['shortForecast'], forecast[1]['detailedForecast'])
+    #data2 = (alerts, name, temp, thumb, short, detailed)
+    
+    
     return((data1, data2))
