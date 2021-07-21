b7ee9c563be500e41d47a3e50bae6919a62e76e2
airD173 <adey1731@gmail.com>
2021-07-21 18:01:43 -0400

    commit on 'alert-development'

--- data.py
+++ data.py
@@ -10,21 +10,18 @@ def main(zipcode):
     latitude = zipdb[zipdb['zip'] == zipcode]['latitude'].values[0]
     longitude = zipdb[zipdb['zip'] == zipcode]['longitude'].values[0]
     coord = requests.get(f'https://api.weather.gov/points/{latitude},{longitude}').json()
-    alertdata = requests.get('https://api.weather.gov/alerts/active?zone=' + requests.get(coord['properties']['county']).json()['properties']['id']).json()
+    alertdata = requests.get('https://api.weather.gov/alerts/active?zone=' + requests.get(coord['properties']['county']).json()['properties']['id']).json()['features']
     forecast = requests.get(coord['properties']['forecast']).json()['properties']['periods']
     
-    if len(alertdata['features']) == 0:
+    if len(alertdata) == 0:
         alerts = None
     else:
-        alerts = alertdata['features']
-            
-
+        alerts = alertdata
     
     data1 = (zipcode, forecast[0]['name'], forecast[0]['temperature'], icon.main(forecast[0]['icon']), forecast[0]['shortForecast'], forecast[0]['detailedForecast'])
     #data1 = (zipcode, name, temp, thumb, short, detailed)
-    
     data2 = (alerts, forecast[1]['name'], forecast[1]['temperature'], icon.main(forecast[1]['icon']), forecast[1]['shortForecast'], forecast[1]['detailedForecast'])
     #data2 = (alerts, name, temp, thumb, short, detailed)
     
-    
     return((data1, data2))
+
